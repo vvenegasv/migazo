af98a246110136d5661df90c432214a94e8611f2
LANET\vvenegas <vladimir.venegas@edenred.com>
2017-06-30 11:55:12 -0400

    First version

---
+++ src/Migazo/MvcExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Hosting;
+using System.Web.Mvc;
+
+namespace Migazo
+{
+    public static class MvcExtensions
+    {
+        /// <summary>
+        /// Wraper to @Url.Content to include file modification date as yyyyMMddHHmmss
+        /// </summary>
+        /// <param name="url">UrlHelper</param>
+        /// <param name="path">Path to file</param>
+        /// <returns>string with file path with version as ~your/file/path?v=date</returns>
+        public static string ContentWithDateParam(this UrlHelper url, string path)
+        {
+            var filePath = HostingEnvironment.MapPath(path);
+            var urlPath = path + "?v=" + new FileInfo(filePath).LastWriteTime.ToString("yyyyMMddHHmmss");
+            return url.Content(urlPath);
+        }
+    }
+}
